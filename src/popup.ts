857a8aec1c8a80506af3d760d78be5f56f09f2c7
Lukas Winkler <git@lw1.at>
2022-05-22 17:09:23 +0200

    update

--- src/popup.ts
+++ src/popup.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import opening_hours from "opening_hours";
 import {toName} from "./utils";
-import { DateTime } from 'luxon';
+import {DateTime} from 'luxon';
 
 export function getPopupText(feature: GeoJSON.Feature) {
     let popuptext = [];
@@ -23,27 +23,32 @@ export function getPopupText(feature: GeoJSON.Feature) {
         popuptext.push("E-Mail: <a href='tel:" + prop.email + "'>" + prop.email + "</a>");
     }
     if (prop.opening_hours) {
-        let oh = new opening_hours(prop.opening_hours, {
-            "address": {
-                "state": "Niederösterreich",
-                "country": "Österreich",
-                "country_code": "at"
+        try {
+            let oh = new opening_hours(prop.opening_hours, {
+                lat: 48.513969429467124,
+                lon: 15.628936886787415,
+                "address": {
+                    "state": "Niederösterreich",
+                    "country_code": "at"
+                }
+            });
+            if (!oh.getUnknown()) {
+                let openText;
+                let change = DateTime.fromJSDate(oh.getNextChange());
+                if (oh.getState()) {
+                    openText = "hat geöffnet<br>schließt ";
+                } else {
+                    openText = "hat geschlossen<br>öffnet ";
+                }
+                if (oh.getNextChange()) {
+                    openText += change.toRelativeCalendar() + " (" + change.toRelative() + ")";
+                } else {
+                    openText += "nie"
+                }
+                popuptext.push(openText);
             }
-        });
-        if (!oh.getUnknown()) {
-            let openText;
-            let change = DateTime.fromJSDate(oh.getNextChange());
-            if (oh.getState()) {
-                openText = "hat geöffnet<br>schließt ";
-            } else {
-                openText = "hat geschlossen<br>öffnet ";
-            }
-            if (oh.getNextChange()) {
-                openText += change.toRelativeCalendar() + " (" + change.toRelative() + ")";
-            } else {
-                openText += "nie"
-            }
-            popuptext.push(openText);
+        } catch (e) {
+            console.warn(e);
         }
     }
     return popuptext.join("<br>");
